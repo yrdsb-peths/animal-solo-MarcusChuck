bd1ce6a85c1991b1e6651b2d26ebe91afe8189dd
MarcusChuck <348580788@gapps.yrdsb.ca>
2025-04-29 09:31:11 -0400

    added fucntion create apple and removing apple after it touches elephant

--- MyWorld.java
+++ MyWorld.java
@@ -7,7 +7,15 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        createApple();
+    }
+    
+    
+    public void createApple()
+    {
         Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
         addObject(apple, 300, 0);
     }
 }

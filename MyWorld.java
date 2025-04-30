57feb16a9d79d3ebbfd3fcea0530fc1ff5d553ba
MarcusChuck <348580788@gapps.yrdsb.ca>
2025-04-30 08:39:16 -0400

    Part 1: created eat where it will remove apple and create another apple, added label to track score for each apple eaten, added game over when apples reaches bottom of the screen

--- MyWorld.java
+++ MyWorld.java
@@ -1,13 +1,31 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    
+    public int score = 0;
+    Label scoreLabel;
+    
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50 , 50);
         createApple();
+    }
+    
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     
@@ -16,6 +34,6 @@ public class MyWorld extends World {
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
-        addObject(apple, 300, 0);
+        addObject(apple, x, y);
     }
 }
